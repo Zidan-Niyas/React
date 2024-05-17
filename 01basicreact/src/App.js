cf7ce39a2fb3164eab09da41cac9dc24082b662e
Zidan <muhdzidan007@gmail.com>
2024-05-17 22:22:16 +0530

    deleted unnecessary files

--- 01basicreact/src/App.js
+++
@@ -1,7 +0,0 @@
-function App() {
-  return (
-    <h1>Chai aur react | Zidan</h1>
-  );
-}
-
-export default App;
--- 01vitereact/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai() {
-    return (
-        <h3>Chai in the evening is a vibe</h3>
-    )
-}
-
-export default Chai
